054801e042be3a57979978e81ebf7b1571e46a34
Sotskin <sotskin@gmail.com>
2020-12-18 20:34:54 -0500

    manage course - student version

--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,4 @@
-// var numDynamicButtons = 0;
-//
-// function createButton() {
-//     numDynamicButtons++;
-//     var newButton = $("<button class='dynamic-button'>")
-//     var newButtonText = "Dynamic Button #" + numDynamicButtons
-//
-//     $(newButton).text(newButtonText)
-//     $("#dynamic-button-container").append(newButton)
-//
-//     $(newButton).click(function(){ alert("Clicked " + newButtonText + "!") })
-// }
-
-// $(document).ready(function(){
-//     $("#dialog").dialog({
-//         autoOpen: false
-//     });
-//     $("#main-button").click(function(){
-//         createButton();
-//     })
-// })
-
-let user = {name: "Sotsu", courses: [{"User Interface": 1}, {"Database Management": 0}]}
+// let user = {name: "Sotsu", courses: [{"User Interface": 1}, {"Database Management": 0}]}
 
 $(function() {
     $("#dialog-manage-course").dialog({
