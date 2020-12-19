4e6790eea03a7648efdea56ccff173b6f959bc8e
Sotskin <sotskin@gmail.com>
2020-12-18 19:57:52 -0500

    dialog overlay, button color, minor fixes

--- js/popup.js
+++ js/popup.js
@@ -24,12 +24,19 @@ let user = {name: "Sotsu", courses: [{"User Interface": 1}, {"Database Managemen
 
 $(function() {
     $("#dialog-manage-course").dialog({
-        autoOpen: false,
+        autoOpen: true,
         dialogClass: 'dialog',
         width: "555px",
         padding: "20px",
+        modal: true,
+        open: function (e, ui) {
+            $('.ui-widget-overlay').bind('click', function () {
+                $("#dialog-manage-course").dialog('close');
+            });
+        }
     });
-    $("#button-manage-course").on("click", function() {
+    $("#button-manage-course").on("click", function(e) {
+        e.preventDefault()
         $("#dialog-manage-course").dialog("open");
     });
 });
