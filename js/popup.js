4d7d6d1936125693b993a4b2b462c700c35e19e3
Derek Jin <sotskin@gmail.com>
2020-12-20 04:59:41 -0500

    working javascript links between all components

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,14 @@
 
 const is_professor = false;
 
+$(window).resize(() => {reCenterDialog()});
+
+const onClickDefault = (actorName) => {
+    $("#"+actorName).dialog('close')
+}
+
 const onClickAddCourse = (actorName) => {
-    console.log(actorName)
+    // console.log(actorName)
     $("#"+actorName).dialog('close')
     $("#dialog-enroll-course").dialog('open')
 }
@@ -27,6 +33,12 @@ $(function() {
             });
         }
     });
+    $("#button-manage-make-appointment").on("click", (e) => {
+        e.preventDefault();
+        $("#dialog-manage-course-student").dialog("close")
+        $("#dialog-make-appointment").dialog("open")
+    })
+
     $("#dialog-manage-course-professor").dialog({
         autoOpen: false,
         dialogClass: 'dialog manage-courses',
@@ -39,6 +51,7 @@ $(function() {
             });
         }
     });
+
     $("#dialog-enroll-course").dialog({
         autoOpen: false,
         dialogClass: 'dialog manage-courses',
@@ -124,6 +137,96 @@ $(function() {
         }
     });
 
+    // Read Event
+    $("#dialog-oh-st").dialog({
+        autoOpen: false,
+        dialogClass: 'dialog office-hour',
+        width: "555px",
+        padding: "20px",
+        modal: true,
+        open: function (e, ui) {
+            $('.ui-widget-overlay').bind('click', function () {
+                $("#dialog-oh-st").dialog('close');
+            });
+        }
+    });
+    $("#note-oh-st").css("display", "none");
+    $("#button-oh-st-remove").css("display", "none");
+    $("#button-oh-st-remove").on("click", (e) => {
+        $("#button-oh-st-add").css("display", "block")
+        $("#note-oh-st").css("display", "none");
+        $("#button-oh-st-remove").css("display", "none");
+    })
+    $("#button-oh-st-add").on("click", (e) => {
+        $("#button-oh-st-add").css("display", "none")
+        $("#note-oh-st").css("display", "block");
+        $("#button-oh-st-remove").css("display", "block");
+    })
+
+    $("#dialog-oh-ins").dialog({
+        autoOpen: false,
+        dialogClass: 'dialog office-hour',
+        width: "555px",
+        padding: "20px",
+        modal: true,
+        open: function (e, ui) {
+            $('.ui-widget-overlay').bind('click', function () {
+                $("#dialog-oh-ins").dialog('close');
+            });
+        }
+    });
+    $("#button-oh-ins-edit").on("click", (e) => {
+        e.preventDefault();
+        $("#dialog-oh-ins").dialog('close')
+        $("#dialog-edit-oh").dialog('open')
+    })
+    $("#button-edit-oh-confirm").on("click", (e) => {
+        e.preventDefault();
+        $("#dialog-edit-oh").dialog('close')
+        $("#dialog-oh-ins").dialog('open')
+    })
+
+    $("#dialog-app-st").dialog({
+        autoOpen: false,
+        dialogClass: 'dialog appointments',
+        width: "555px",
+        padding: "20px",
+        modal: true,
+        open: function (e, ui) {
+            $('.ui-widget-overlay').bind('click', function () {
+                $("#dialog-app-st").dialog('close');
+            });
+        }
+    });
+    $("#button-app-st-edit").on("click", (e) => {
+        e.preventDefault();
+        $("#dialog-app-st").dialog("close")
+        $("#dialog-make-appointment").dialog("open")
+    })
+
+    $("#dialog-app-ins").dialog({
+        autoOpen: false,
+        dialogClass: 'dialog appointments',
+        width: "555px",
+        padding: "20px",
+        modal: true,
+        open: function (e, ui) {
+            $('.ui-widget-overlay').bind('click', function () {
+                $("#dialog-app-ins").dialog('close');
+            });
+        }
+    });
+    $("#button-app-ins-edit").on("click", (e) => {
+        e.preventDefault();
+        $("#dialog-app-ins").dialog("close")
+        $("#dialog-edit-appointment").dialog("open")
+    })
+    $("#button-edit-app-confirm").on("click", (e) => {
+        e.preventDefault();
+        $("#dialog-edit-appointment").dialog('close')
+        $("#dialog-app-ins").dialog('open')
+    })
+
     // Buttons
     $("#button-manage-course").on("click", function(e) {
         e.preventDefault()
@@ -154,4 +257,22 @@ $(function() {
         e.preventDefault()
         $("#dialog-edit-oh").dialog("open");
     });
+
+    $("#button-oh-st-not-joined").on("click", function(e) {
+        e.preventDefault()
+        $("#dialog-oh-st").dialog("open");
+    });
+    $("#button-oh-ins").on("click", function(e) {
+        e.preventDefault()
+        $("#dialog-oh-ins").dialog("open");
+    });
+    $("#button-app-st").on("click", function(e) {
+        e.preventDefault()
+        $("#dialog-app-st").dialog("open");
+    });
+    $("#button-app-ins").on("click", function(e) {
+        e.preventDefault()
+        $("#dialog-app-ins").dialog("open");
+    });
+
 });
